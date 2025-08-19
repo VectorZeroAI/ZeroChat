c8633e006998ff20aa0fc7ca7cea6b3d4cb7ca51
Zero <vectorzeroai@gmail.com>
2025-08-19 19:10:39 +0200

    Create ZeroMain.py

---
+++ ZeroMain.py
@@ -0,0 +1,19 @@
+#ZeroMain.py
+"""
+This is the main chatloop of the app. 
+It can be run for CL I/O, or the GUI can use it as backend.
+"""
+from config import 
+
+
+def call_LLM(prompt):
+    if 
+
+
+
+
+
+
+
+if __name__ = __main__:
+    #the chatloop goes here
