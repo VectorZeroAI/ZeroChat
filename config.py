60f456de59710014b484d3b5c3ce83f01ec609b3
Zero <vectorzeroai@gmail.com>
2025-08-19 19:06:05 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+LLM_SOURSE = "OPENROUTER" #This code supports:
+#Openrouter API --> input "OPENROUTER"
+#Local model --> input "LOCAL"
+#Gemini API --> input "GEMINI"
+
+
+#Uncomment the thing you are using, comment all the other things if you wish to.
+OPENROUTER_API_KEY = "input your API here"
+# LOCAL_MODEL_PATH = "input your Path to the model here"
+# GEMINI_API_KEY = "input your API key here"
+
