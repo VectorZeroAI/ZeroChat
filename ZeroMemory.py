7e723925e7b32877d261e98a58f86665d92d0eab
Zero <vectorzeroai@gmail.com>
2025-08-20 13:26:54 +0200

    Update ZeroMemory.py

--- ZeroMemory.py
+++ ZeroMemory.py
@@ -1,7 +1,10 @@
-
+"""
+A placeholder for now
+Later is gonna be expanded into the most important part of the Application.
+"""
 
 class memory:
-    def __init__:
+    def __init__(self):
         self.memory = "placeholder for now"
 
     def full(self):
