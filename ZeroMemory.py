e9c916103a1dc6ad28936cd60cc72bc4f1b422f2
Zero <vectorzeroai@gmail.com>
2025-08-20 13:42:07 +0200

    Update ZeroMemory.py

--- ZeroMemory.py
+++ ZeroMemory.py
@@ -2,10 +2,14 @@
 A placeholder for now
 Later is gonna be expanded into the most important part of the Application.
 """
+from ZeroIndentity import indentity
 
 class memory:
     def __init__(self):
-        self.memory = "placeholder for now"
+        self.memory = None
+        self.querrie = None
 
     def full(self):
+        
+
         return self.memory
